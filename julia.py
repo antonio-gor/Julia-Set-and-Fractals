8323cd0a1061215154bd9d8e6f0c24a62c720577
Antonio Gordillo Toledo <antonio.gor@hotmail.com>
2019-10-15 18:16:27 -0700

    Modify code and filename structure

--- julia.py
+++ julia.py
@@ -1,15 +1,13 @@
-# Python code for Julia Fractal
+# Packages to be used
 from PIL import Image
 import time
 import sys
 
-# driver function
 def julia(w=1920, h=1080, zoom=1, R=2, cX=-0.7702, cY=0.1442,
-          moveX=0.0, moveY=0.0, maxIter=255, filename="desc", disp=False):
+          moveX=0.0, moveY=0.0, iterations=255, filename="desc", disp=False):
     '''
     Renders a fractal according to the Julia set. Can display the
     rendered image and/or save it as a png file.
-
     Args:
         w: image width
         h: image height
@@ -19,42 +17,42 @@ def julia(w=1920, h=1080, zoom=1, R=2, cX=-0.7702, cY=0.1442,
         cY: constant along the y axis
         moveX: shift the image along the x axis
         moveY: shift the image along the y axis
-        maxIter: maximum number of iterations
+        iterations: maximum number of iterations
         filename: default filename is a description of the settings
         disp: display the fractal; default is False
     '''
 
     # Checking if descriptive filename was selected
     if filename == "desc":
-        filename = "."+str(w)+"."+str(h)+"."+str(zoom)+"." \
-        +str(cX)+"."+str(cY)+"."+str(moveX)+"."+str(moveY)+"."+str(maxIter)
+        filename = str(w)+"_"+str(h)+"_"+str(zoom)+"_" \
+        +str(cX)+"_"+str(cY)+"_"+str(moveX)+"_"+str(moveY)+"_"+str(iterations)
 
     # Creating new bitmap in RGB mode
     bitmap = Image.new(mode="RGB", size=(w, h), color="white")
 
-    # Allocating the storage for the image and loading the pixel data.
+    # Allocating the storage for the image and loading the pixel data
     pix = bitmap.load()
 
     for x in range(w):
         for y in range(h):
+            iter = iterations
             zx = 1.5*(x - w/2)/(0.5*zoom*w) + moveX
             zy = 1.0*(y - h/2)/(0.5*zoom*h) + moveY
-            iteration = maxIter
-            while zx**2 + zy**2 < R**2 and iteration > 1:
+            while zx**2 + zy**2 < R**2 and iter > 1:
                 zx, zy = zx**2 - zy**2 + cX, 2.0*zx*zy + cY
-                iteration -= 1
+                iter -= 1
 
             # Converting byte to RGB (3 bytes)
-            pix[x,y] = (iteration << 21) + (iteration << 10) + iteration*8
+            pix[x,y] = (iter << 21) + (iter << 10) + iter*8
 
     # Display the rendered fractal
     if disp:
         bitmap.show()
 
     # Saving the image
-    bitmap.save("img/julia" + str(filename) + ".png")
+    bitmap.save("img/julia_" + str(filename) + ".png")
 
-def runtype(args, ):
+def runtype(args):
     ''' Checks for command line args or does a custom run '''
     if args[0] == 'sample':
         julia()
@@ -66,8 +64,7 @@ def runtype(args, ):
             julia(cX=-0.8702+i/1000, filename=i, disp=False)
 
 def begin():
-    # Command line args are optional
-    # arg0, arg1, ag2 = cX, cY, filename
+    ''' Command line args are optional; arg0, arg1, ag2 = cX, cY, filename '''
     start = time.time()
     runtype(sys.argv[1:])
     end = time.time()
